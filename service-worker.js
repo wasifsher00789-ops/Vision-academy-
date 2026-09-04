665fe3935d6a7b7428f88c839e3735fcc3831ec6
wasifsher00789-ops <wasifsher00789@gmail.com>
2026-09-04 15:22:18 +0500

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-
-const CACHE_NAME = "vision-academy-admin-v1";
-const FILES = ["./index.html","./manifest.json"];
-self.addEventListener("install", e => {
- e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));
-});
-self.addEventListener("fetch", e => {
- e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
-});

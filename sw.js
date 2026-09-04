652a9f828abc1e6d9f3d328b66d4daedc72efdab
wasifsher00789-ops <wasifsher00789@gmail.com>
2026-09-04 15:59:07 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="vision-academy-v1";
-const FILES=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));

4b9e7d8608a161476f90682fcc3014e3e1f3e7e2
wasifsher00789-ops <wasifsher00789@gmail.com>
2026-09-04 08:29:26 +0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+
+const CACHE_NAME = 'vision-academy-admin-v1';
+const FILES = ['./index.html','./manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
+});
